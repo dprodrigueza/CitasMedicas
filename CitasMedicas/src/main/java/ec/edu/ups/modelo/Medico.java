ef8600e13a021e3e5557827321466a1ea83408cd
Geovanny Duchitanga Abad <gioabad210@hotmail.com>
2019-12-18 14:31:59 -0500

    Update Medico.java
    Commit 4

--- CitasMedicas/src/main/java/ec/edu/ups/modelo/Medico.java
+++ CitasMedicas/src/main/java/ec/edu/ups/modelo/Medico.java
@@ -29,8 +29,50 @@ public class Medico {
        this.med_password =  med_password;
        this.med_persona_id = med_persona_id;
     }
+
+    public int getMed_id() {
+        return med_id;
+    }
+
+    public void setMed_id(int med_id) {
+        this.med_id = med_id;
+    }
+
+    public int getMed_numeroConsultorio() {
+        return med_numeroConsultorio;
+    }
+
+    public void setMed_numeroConsultorio(int med_numeroConsultorio) {
+        this.med_numeroConsultorio = med_numeroConsultorio;
+    }
+
+    public String getMed_usuario() {
+        return med_usuario;
+    }
+
+    public void setMed_usuario(String med_usuario) {
+        this.med_usuario = med_usuario;
+    }
+
+    public String getMed_password() {
+        return med_password;
+    }
+
+    public void setMed_password(String med_password) {
+        this.med_password = med_password;
+    }
+
+    public int getMed_persona_id() {
+        return med_persona_id;
+    }
+
+    public void setMed_persona_id(int med_persona_id) {
+        this.med_persona_id = med_persona_id;
+    }
   
   
-  
+   public String mostrarInfoMedico() {
+        return "Medico{" + "med_id=" + med_id + ", med_numeroConsultorio=" + med_numeroConsultorio + ", med_usuario=" + med_usuario + ", med_password=" + med_password + ", med_persona_id=" + med_persona_id +  '}';
+    }
     
 }
