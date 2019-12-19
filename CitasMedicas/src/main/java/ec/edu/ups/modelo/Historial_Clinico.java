2f247dd636da08e336e1257b833be62b57007627
DamianSumba <49218606+DamianSumba@users.noreply.github.com>
2019-12-18 23:52:50 -0500

    Update Historial_Clinico.java
    creacion clase historialClinico

--- CitasMedicas/src/main/java/ec/edu/ups/modelo/Historial_Clinico.java
+++ CitasMedicas/src/main/java/ec/edu/ups/modelo/Historial_Clinico.java
@@ -5,12 +5,58 @@
  */
 package ec.edu.ups.modelo;
 
+import java.util.Date;
+
 /**
  *
  * @author Damián Sumba
  */
 public class Historial_Clinico {
-    
+
     private int codigoHistorial;
+    private String descripcion;
+    private String tipo;
+    private Date fecha;
+    // private listaDiagnostico list<>;
+
+    public Historial_Clinico() {
+    }
+
+    public int getCodigoHistorial() {
+        return codigoHistorial;
+    }
+
+    public void setCodigoHistorial(int codigoHistorial) {
+        this.codigoHistorial = codigoHistorial;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Historial_Clinico{" + "codigoHistorial=" + codigoHistorial + ", descripcion=" + descripcion + ", tipo=" + tipo + ", fecha=" + fecha + '}';
+    }
     
 }
