3c2d7d541ee99f2bc7fa72233a974e1001e2029d
Geovanny Duchitanga Abad <gioabad210@hotmail.com>
2019-12-20 10:59:17 -0500

    Create DiagnosticoTest.java

---
+++ CitasMedicas/src/test/java/ec/edu/ups/modelo/DiagnosticoTest.java
@@ -0,0 +1,324 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.modelo;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author GeovannyAbad
+ */
+public class DiagnosticoTest {
+    
+    public DiagnosticoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getCodigoDiagnostico method, of class Diagnostico.
+     */
+    @Test
+    public void testGetCodigoDiagnostico() {
+        System.out.println("getCodigoDiagnostico");
+        Diagnostico instance = new Diagnostico();
+        int expResult = 0;
+        int result = instance.getCodigoDiagnostico();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCodigoDiagnostico method, of class Diagnostico.
+     */
+    @Test
+    public void testSetCodigoDiagnostico() {
+        System.out.println("setCodigoDiagnostico");
+        int codigoDiagnostico = 0;
+        Diagnostico instance = new Diagnostico();
+        instance.setCodigoDiagnostico(codigoDiagnostico);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEnfermedadActual method, of class Diagnostico.
+     */
+    @Test
+    public void testGetEnfermedadActual() {
+        System.out.println("getEnfermedadActual");
+        Diagnostico instance = new Diagnostico();
+        String expResult = "";
+        String result = instance.getEnfermedadActual();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEnfermedadActual method, of class Diagnostico.
+     */
+    @Test
+    public void testSetEnfermedadActual() {
+        System.out.println("setEnfermedadActual");
+        String enfermedadActual = "";
+        Diagnostico instance = new Diagnostico();
+        instance.setEnfermedadActual(enfermedadActual);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getBoca method, of class Diagnostico.
+     */
+    @Test
+    public void testGetBoca() {
+        System.out.println("getBoca");
+        Diagnostico instance = new Diagnostico();
+        String expResult = "";
+        String result = instance.getBoca();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setBoca method, of class Diagnostico.
+     */
+    @Test
+    public void testSetBoca() {
+        System.out.println("setBoca");
+        String boca = "";
+        Diagnostico instance = new Diagnostico();
+        instance.setBoca(boca);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTorax method, of class Diagnostico.
+     */
+    @Test
+    public void testGetTorax() {
+        System.out.println("getTorax");
+        Diagnostico instance = new Diagnostico();
+        String expResult = "";
+        String result = instance.getTorax();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setTorax method, of class Diagnostico.
+     */
+    @Test
+    public void testSetTorax() {
+        System.out.println("setTorax");
+        String torax = "";
+        Diagnostico instance = new Diagnostico();
+        instance.setTorax(torax);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAbdomen method, of class Diagnostico.
+     */
+    @Test
+    public void testGetAbdomen() {
+        System.out.println("getAbdomen");
+        Diagnostico instance = new Diagnostico();
+        String expResult = "";
+        String result = instance.getAbdomen();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setAbdomen method, of class Diagnostico.
+     */
+    @Test
+    public void testSetAbdomen() {
+        System.out.println("setAbdomen");
+        String abdomen = "";
+        Diagnostico instance = new Diagnostico();
+        instance.setAbdomen(abdomen);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getExtremidades method, of class Diagnostico.
+     */
+    @Test
+    public void testGetExtremidades() {
+        System.out.println("getExtremidades");
+        Diagnostico instance = new Diagnostico();
+        String expResult = "";
+        String result = instance.getExtremidades();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setExtremidades method, of class Diagnostico.
+     */
+    @Test
+    public void testSetExtremidades() {
+        System.out.println("setExtremidades");
+        String extremidades = "";
+        Diagnostico instance = new Diagnostico();
+        instance.setExtremidades(extremidades);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getRegionPerineal method, of class Diagnostico.
+     */
+    @Test
+    public void testGetRegionPerineal() {
+        System.out.println("getRegionPerineal");
+        Diagnostico instance = new Diagnostico();
+        String expResult = "";
+        String result = instance.getRegionPerineal();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setRegionPerineal method, of class Diagnostico.
+     */
+    @Test
+    public void testSetRegionPerineal() {
+        System.out.println("setRegionPerineal");
+        String regionPerineal = "";
+        Diagnostico instance = new Diagnostico();
+        instance.setRegionPerineal(regionPerineal);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getValoracionNeurologica method, of class Diagnostico.
+     */
+    @Test
+    public void testGetValoracionNeurologica() {
+        System.out.println("getValoracionNeurologica");
+        Diagnostico instance = new Diagnostico();
+        String expResult = "";
+        String result = instance.getValoracionNeurologica();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setValoracionNeurologica method, of class Diagnostico.
+     */
+    @Test
+    public void testSetValoracionNeurologica() {
+        System.out.println("setValoracionNeurologica");
+        String valoracionNeurologica = "";
+        Diagnostico instance = new Diagnostico();
+        instance.setValoracionNeurologica(valoracionNeurologica);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getIce10 method, of class Diagnostico.
+     */
+    @Test
+    public void testGetIce10() {
+        System.out.println("getIce10");
+        Diagnostico instance = new Diagnostico();
+        String expResult = "";
+        String result = instance.getIce10();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setIce10 method, of class Diagnostico.
+     */
+    @Test
+    public void testSetIce10() {
+        System.out.println("setIce10");
+        String ice10 = "";
+        Diagnostico instance = new Diagnostico();
+        instance.setIce10(ice10);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTratamientos method, of class Diagnostico.
+     */
+    @Test
+    public void testGetTratamientos() {
+        System.out.println("getTratamientos");
+        Diagnostico instance = new Diagnostico();
+        String expResult = "";
+        String result = instance.getTratamientos();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setTratamientos method, of class Diagnostico.
+     */
+    @Test
+    public void testSetTratamientos() {
+        System.out.println("setTratamientos");
+        String tratamientos = "";
+        Diagnostico instance = new Diagnostico();
+        instance.setTratamientos(tratamientos);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class Diagnostico.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Diagnostico instance = new Diagnostico();
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
