a912c5843a32c36718ca17bb3350ff369bc30115
Geovanny Duchitanga Abad <gioabad210@hotmail.com>
2019-12-20 10:00:32 -0500

    Create MedicoTest.java

---
+++ CitasMedicas/src/test/java/ec/edu/ups/controlador/MedicoTest.java
@@ -0,0 +1,80 @@
+package ec.edu.ups.controlador;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MedicoTest {
+
+	public int med_id;
+    public int med_numeroConsultorio;
+    public String med_usuario;
+    public String med_password;
+  public int med_persona_id;
+  
+  
+  public void Medico(int med_id, int med_numeroConsultorio, String med_usuario, String med_password, int med_persona_id) {
+        this.med_id = med_id;
+       this.med_numeroConsultorio = med_numeroConsultorio;
+       this.med_usuario = med_usuario;
+       this.med_password =  med_password;
+       this.med_persona_id = med_persona_id;
+    }
+
+    public void Medico() {
+      
+    }
+
+
+ 
+
+    public int getMed_id() {
+        return med_id;
+    }
+
+    public void setMed_id(int med_id) {
+        this.med_id = med_id;
+    }
+
+    public int getMed_numeroConsultorio() {
+        return med_numeroConsultorio;
+    }
+
+    public void setMed_numeroConsultorio(int med_numeroConsultorio) {
+        this.med_numeroConsultorio = med_numeroConsultorio;
+    }
+
+    public String getMed_usuario() {
+        return med_usuario;
+    }
+
+    public void setMed_usuario(String med_usuario) {
+        this.med_usuario = med_usuario;
+    }
+
+    public String getMed_password() {
+        return med_password;
+    }
+
+    public void setMed_password(String med_password) {
+        this.med_password = med_password;
+    }
+
+    public int getMed_persona_id() {
+        return med_persona_id;
+    }
+
+    public void setMed_persona_id(int med_persona_id) {
+        this.med_persona_id = med_persona_id;
+    }
+  
+  
+   public String mostrarInfoMedico() {
+        return "Medico{" + "med_id=" + med_id + ", med_numeroConsultorio=" + med_numeroConsultorio + ", med_usuario=" + med_usuario + ", med_password=" + med_password + ", med_persona_id=" + med_persona_id +  '}';
+    }
+    
+	
+	
+	
+
+}
