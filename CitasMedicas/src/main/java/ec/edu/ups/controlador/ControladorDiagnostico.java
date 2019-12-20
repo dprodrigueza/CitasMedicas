4d9e1a508f81a29f4203cc891679b968b258bfb9
DamianSumba <49218606+DamianSumba@users.noreply.github.com>
2019-12-20 11:16:54 -0500

    creacion clase conexion

--- CitasMedicas/src/main/java/ec/edu/ups/controlador/ControladorDiagnostico.java
+++ CitasMedicas/src/main/java/ec/edu/ups/controlador/ControladorDiagnostico.java
@@ -5,7 +5,11 @@
  */
 package ec.edu.ups.controlador;
 
+import ec.edu.ups.conexion.conexion;
 import ec.edu.ups.modelo.Diagnostico;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import java.util.List;
 
@@ -15,9 +19,25 @@ import java.util.List;
  */
 public class ControladorDiagnostico {
     
+    private Statement statement;
+    private ResultSet r;
+    private conexion conexion;
+    private PreparedStatement ps;
+
+    Diagnostico diagnostico = new Diagnostico();
+
     private List<Diagnostico> listaDiagnstico;
-    
-   public void registrarDiagnostico (Diagnostico diagnostico){}
-    
-   
+
+    public void registrarDiagnostico(Diagnostico diagnostico) {
+//        String sql = "INSERT INTO Diagnostico VALUES ( " + p.getPa_id() + "'" + p.getPa_religion() + "', '" + p.getPa_recidencia() + "' , '"
+//                + p.getPa_procedencia() + "', '" + p.getPa_ocupacion() + "', '" + p.getPersona() + "' ,'" + ");";
+//
+//        System.out.println("" + sql);
+        
+        String sql = "INSERT INTO Diagnostico VALUES ( " + diagnostico.getCodigoDiagnostico()+ "'" + diagnostico.getEnfermedadActual() + "', '" + diagnostico.getBoca() + "' , '"
+                + diagnostico.getTorax() + "', '" + diagnostico.getAbdomen() + "', '" + diagnostico.getExtremidades() + "' ,'" +  diagnostico.getRegionPerineal() + "','" + ");";
+
+        System.out.println("" + sql);
+    }
+
 }
