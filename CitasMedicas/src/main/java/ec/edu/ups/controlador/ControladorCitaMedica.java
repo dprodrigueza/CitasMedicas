cc3162300f2818409d2b531b0e5d9a8382a2ae77
DamianSumba <49218606+DamianSumba@users.noreply.github.com>
2020-02-05 13:02:38 -0500

    modifcacion citasMedicas

--- CitasMedicas/src/main/java/ec/edu/ups/controlador/ControladorCitaMedica.java
+++ CitasMedicas/src/main/java/ec/edu/ups/controlador/ControladorCitaMedica.java
@@ -25,22 +25,20 @@ import javax.swing.JOptionPane;
  * @author VIVIANA
  */
 public class ControladorCitaMedica {
-    
+
     private Connection con;
     private List<CitaMedica> listacitaMedica;
     private Statement statement;
     private ResultSet r;
     private conexion conexion;
     private PreparedStatement ps;
-    
-    
-    public ControladorCitaMedica(){
-        listacitaMedica =  new ArrayList<CitaMedica>();
-       
+
+    public ControladorCitaMedica() {
+        
     }
-   
+
     public void crearCitaMedica(CitaMedica citaMedica) {
-        ControladorMedico controladorMedico =  new ControladorMedico();
+        ControladorMedico controladorMedico = new ControladorMedico();
         ControladorPaciente controladorPaciente = new ControladorPaciente();
         String sql = "INSERT INTO citasMedicas (codigoCita, fechaCita, horaCita, paciente, medico, motivoCita " + ")" + " values (?,?,?,?,?,?);";
         try {
@@ -52,63 +50,45 @@ public class ControladorCitaMedica {
             ps.setInt(4, citaMedica.getPaciente().getPa_id());
             ps.setInt(5, citaMedica.getMedico().getMedico_id());
             ps.setString(6, citaMedica.getCita_motivo());
-           
+
         } catch (Exception e) {
             System.out.println("error " + e);
             JOptionPane.showMessageDialog(null, "Los Datos no han sido Guardados");
         } finally {
-        conexion.desconectar();
+            conexion.desconectar();
         }
     }
-    
-    
+
     public void eliminarCitaMedica(CitaMedica citaMedica, int id) {
         try {
-            statement =  conexion.getConexion().createStatement(r.TYPE_SCROLL_SENSITIVE, r.CONCUR_UPDATABLE);
+            statement = conexion.getConexion().createStatement(r.TYPE_SCROLL_SENSITIVE, r.CONCUR_UPDATABLE);
             statement.executeUpdate("DELETE FROM citasMedicas WHERE id =" + id + ";");
             JOptionPane.showMessageDialog(null, "cita medica eliminada ");
         } catch (Exception e) {
-            System.out.println("problema al conectarse a la BD" + e );
+            System.out.println("problema al conectarse a la BD" + e);
             JOptionPane.showMessageDialog(null, "cita medica no eliminada");
         }
-     
+
     }
-     public void modificarCitaMedica(CitaMedica citaMedica, int id) {
-         
-         try {
-             statement =  conexion.getConexion().createStatement(r.TYPE_SCROLL_SENSITIVE, r.CONCUR_UPDATABLE);
-             statement.executeUpdate("UPDATE citasMedicas set cita_id  = '" + citaMedica.getCita_id()
-            + "', cita_fecha = '" + citaMedica.getCita_fecha()
-            + "', cita_hora = '" + citaMedica.getCita_hora()
-            + "', cita_motivo = '"+ citaMedica.getCita_motivo()
-            + "', cita_medico = '" + citaMedica.getMedico()
-            + "', cita_paciente = '" + citaMedica.getPaciente()
-            + "WHERE cita_id = '" + id + "';");
-             JOptionPane.showMessageDialog(null, "Citas Medicas Actualizados con Exito");
-                     
-         } catch (Exception e) {
-             System.out.println("error" + e);
-             JOptionPane.showMessageDialog(null, "Datos no actualizados");
-         }
-         
-         
-    String sql = "UPDATE cita_medica  SET   cita_id  = '" + cm.getCita_id()
-            + "', cita_fecha = '" + cm.getCita_fecha()
-            + "', cita_hora = '" + cm.getCita_hora()
-            + "', cita_motivo = '"+ cm.getCita_motivo()
-            + "', cita_medico = '" + cm.getMedico()
-            + "', cita_paciente = '" + cm.getPaciente()
-          
-                + " WHERE cita_id ='" + id + "';";
-//        System.out.println(sql);
-//        try {
-//            conectar();
-//            Statement sta = con.createStatement();
-//            sta.executeUpdate(sql);
-//            desconectar();
-//        } catch (SQLException ex) {
-//            System.out.println("Error  " + ex.getMessage());
-//        }
-  }
-  
+
+    public void modificarCitaMedica(CitaMedica citaMedica, int id) {
+
+        try {
+            statement = conexion.getConexion().createStatement(r.TYPE_SCROLL_SENSITIVE, r.CONCUR_UPDATABLE);
+            statement.executeUpdate("UPDATE citasMedicas set cita_id  = '" + citaMedica.getCita_id()
+                    + "', cita_fecha = '" + citaMedica.getCita_fecha()
+                    + "', cita_hora = '" + citaMedica.getCita_hora()
+                    + "', cita_motivo = '" + citaMedica.getCita_motivo()
+                    + "', cita_medico = '" + citaMedica.getMedico()
+                    + "', cita_paciente = '" + citaMedica.getPaciente()
+                    + "WHERE cita_id = '" + id + "';");
+            JOptionPane.showMessageDialog(null, "Citas Medicas Actualizados con Exito");
+
+        } catch (Exception e) {
+            System.out.println("error" + e);
+            JOptionPane.showMessageDialog(null, "Datos no actualizados");
+        }
+
+    }
+
 }
